ea7fbe917a69ed176e41c6c2cb8d952a5650ccc4
tazmi <tazmi@VADIM>
2024-06-01 21:28:16 +0700

    Добавьте файлы проекта.

---
+++ AreaFigures/Area.cs
@@ -0,0 +1,56 @@
+﻿namespace AreaFigures
+{
+    public interface IShape // Вычисление площади фигуры без знания типа фигуры в compile-time и легкость добавления других фигур путем реализации интерфейса
+    {
+        double CalcArea();
+    }
+    public class Circle : IShape
+    {
+        public double Radius { get; }
+        public Circle(double radius)
+        {
+            if (radius < 0) throw new Exception("Радиус должен быть больше 0");
+            Radius = radius;
+        }
+        public double CalcArea() { return Math.PI * Math.Pow(Radius,2); }
+    }
+    public class Triangle : IShape
+    {
+        public double sideA { get; }
+        public double sideB { get; }
+        public double sideC { get; }
+        public Triangle(double sideA, double sideB, double sideC)
+        {
+            if (sideA <= 0 || sideB <= 0 || sideC <= 0)
+            {
+                throw new ArgumentException("Все стороны должны быть больше 0");
+            }
+
+            if (sideA + sideB <= sideC || sideA + sideC <= sideB || sideB + sideC <= sideA)
+            {
+                throw new ArgumentException("Сумма двух любых сторон должна быть больше третей");
+            }
+            this.sideC = sideC;
+            this.sideA = sideA;
+            this.sideB = sideB;
+        }
+        public double CalcArea()
+        {
+            double p = (sideC+sideA+sideB)/2;
+            return Math.Sqrt(p*(p-sideB)*(p-sideA)*(p-sideC));
+        }
+        public bool IsRightTriangle() // Проверку на то, является ли треугольник прямоугольным 
+        {
+            double[] a = [sideA,sideB,sideC];
+            Array.Sort(a);
+            return (a[2] * a[2]) == (a[1] * a[1] + a[0] * a[0]);
+        }
+    }
+    public static class ShapeAreaCalculator 
+    {
+        public static double CalculateArea(IShape shape)
+        {
+            return shape.CalcArea();
+        }
+    }
+}
---
+++ AreaFiguresTests/AreaFigures.Tests.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AreaFigures;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AreaFigures.Tests
+{
+    [TestClass()]
+    public class CircleTests
+    {
+        [TestMethod()]
+        public void GetArea_WithValidRadius_ShouldReturnCorrectArea()
+        {
+            double radius = 5;
+            Circle circle = new Circle(radius);
+            double expectedArea = Math.PI * Math.Pow(radius, 2);
+            double actualArea = circle.CalcArea();
+            Assert.AreEqual(expectedArea, actualArea);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Constructor_WithNonPositiveRadius_ShouldThrowException()
+        {
+            new Circle(-1);
+        }
+    }
+    [TestClass()]
+    public class TriangleTests
+    {
+        [TestMethod()]
+        public void TriangleTest()
+        {
+            double sideA = 3;
+            double sideB = 4;
+            double sideC = 5;
+            Triangle triangle = new Triangle(sideA, sideB, sideC);
+            double expectedArea = 6;
+            double actualArea = triangle.CalcArea();
+            Assert.AreEqual(expectedArea, actualArea);
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Constructor_WithInvalidSides_ShouldThrowArgumentException()
+        {
+            new Triangle(1, 1, 3);
+        }
+
+        [TestMethod()]
+        public void IsRightAngled_WithNonRightAngledTriangle_ShouldReturnFalse()
+        {
+            Triangle triangle = new Triangle(3, 4, 6);
+            bool isRight = triangle.IsRightTriangle();
+            Assert.IsFalse(isRight);
+        }
+    }
+    [TestClass()]
+    public class ShapeAreaCalculatorTests
+    {
+        [TestMethod()]
+        public void CalculateArea_WithCircle_ShouldReturnCorrectArea()
+        {
+            IShape circle = new Circle(11);
+            double expectedArea = Math.PI * 121;
+            double actualArea = ShapeAreaCalculator.CalculateArea(circle);
+            Assert.AreEqual(expectedArea, actualArea);
+        }
+
+        [TestMethod()]
+        public void CalculateArea_WithTriangle_ShouldReturnCorrectArea()
+        {
+            IShape triangle = new Triangle(3, 4, 5);
+            double expectedArea = 6;
+            double actualArea = ShapeAreaCalculator.CalculateArea(triangle);
+            Assert.AreEqual(expectedArea, actualArea);
+        }
+    }
+}
